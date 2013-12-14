88fa8e84964e60aec078e2d9028f9cd0a72530a1
Sunil Vignesh R <sunilvigneshr@Sunil-Vignesh-R.local>
2013-12-15 02:32:07 +0530

    Controller for street - wip

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ from flask import render_template
 from flask.ext.pymongo import PyMongo
 
 
-app = Flask(__name__)
+app = Flask("sakti_kodu")
 mongo = PyMongo(app)
 
 @app.route("/street/<street_name>")
-def street(street_name):	    
-    return render_template('index.html', name="street_name")
+def street(street_name):
+	pipeline = [{"$match":{"street":street_name}},{"$group":{"_id":"street","total":{"$sum":1},"energy_dist":{"$addToSet": "$energy"}}}, { "$project" : {
+        "streetlights" : "$total" ,
+        "total_energry" : "$energy" ,
+        "total_power" : "$power",
+        "energy_dist" : "$energy_dist"
+    }}]
+	
+	data_agg = mongo.db.streetlights.aggregate(pipeline)
+	print str(data_agg["result"])
+	return render_template('index.html', aggregate = data_agg["result"])
 
 @app.route("/ward/<ward_number>")
 def ward(ward_number):
