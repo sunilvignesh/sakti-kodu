ffcca9341d6f9d160768d04dee5a06deb29b5fe8
Arvind Ram A <arvindr@INarvindr.local>
2013-12-15 03:06:52 +0530

    thookam varuthu

---
+++ static/js/charts.js
@@ -0,0 +1,38 @@
+//Flot Line Chart with Tooltips
+$(document).ready(function(){
+	console.log("document ready");
+	var offset = 0;
+	plot();
+	function plot(){
+		var sin = [];
+		for (var i = 0; i < 12; i += 0.2) {
+			sin.push([i, Math.sin(i + offset)]);
+		}
+	
+		var options = {
+			series: {
+				lines: { show: true },
+				points: { show: true }
+			},
+			grid: {
+				hoverable: true //IMPORTANT! this is needed for tooltip to work
+			},
+			yaxis: { min: -1.2, max: 1.2 },
+			tooltip: true,
+			tooltipOpts: {
+				content: "'%s' of %x.1 is %y.4",
+				shifts: {
+					x: -60,
+					y: 25
+				}
+			}
+		};
+	
+		var plotObj = $.plot( $("#flot-chart-line"),
+			[ { data: sin, label: "x-axis: Time in seconds, y-axis: Energy(kWh)"}],
+			options );
+	}
+});
+
+
+
